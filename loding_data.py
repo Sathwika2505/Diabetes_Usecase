5f2dbda3892f27b6eb45392bac4c70e0183397f7
SathwikaGopishetty <111847859+SathwikaGopishetty@users.noreply.github.com>
2023-08-17 12:50:44 +0530

    Diabetes_next

--- loding_data.py
+++ loding_data.py
@@ -1,16 +1,3 @@
-import pymysql
+import pandas as pd
 
-connector = pymysql.connect(
-    host='localhost',
-        user='root', 
-        password = "1234",
-        db='dia',
-) 
-
-cur = connector.cursor()
-cur.execute('select * from dia.diabetes_prediction_dataset')
-output = cur.fetchall()
-column_names = [description[0] for description in cur.description]
-print("Column Names:", column_names)
-
-print(type(output))
+df = pd.read_csv("diabetes_prediction_dataset.csv")
