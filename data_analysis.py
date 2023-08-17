20aaa10e068444077575d488d3db6438759621a3
SathwikaGopishetty <111847859+SathwikaGopishetty@users.noreply.github.com>
2023-08-17 12:54:26 +0530

    Next_commit

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from loding_data import output, column_names
+from loding_data import df
 
 
-df = pd.DataFrame(output, columns = column_names )
 df.head()
 
 print("total number of null vlaues form the data set --------", df.isnull().sum().sum())
